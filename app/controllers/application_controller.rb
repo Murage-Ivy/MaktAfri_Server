210847f139f9c5b674518c9045c2b9ab59b10453
RachaelMuriithi <rachael.mureithi@student.moringaschool.com>
2022-11-09 21:27:23 +0300

    author name params  re-edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,33 +9,78 @@ class ApplicationController < Sinatra::Base
   # Gets all books from the database
   get "/books" do
     books = Book.all
-    books.to_json({ include: :author })
+    books.to_json(include: [:author, reviews: {only: :star_rating}])
   end
 
   get "/books/:id" do
+    book = Book.find(params[:id])
+    book.to_json( include: [:author, :reviews] )
   end
 
   post "/books" do
+    author = Author.find_or_create_by(name: params[:author])
+    book = Book.create(title: params[:title],
+                       category: params[:category],
+                       description: params[:description],
+                       published_date: params[:published_date],
+                       image_url: params[:image_url],
+                       author_id: author.id
+                       )
+                      
+
+    book.to_json
   end
 
   patch "/books/:id" do
+    book = Book.find(params[:id])
+    book.update(title: params[:title],
+                category: params[:category],
+                description: params[:description],
+                published_date: params[:published_date],
+                image_url: params[:image_url],
+                author_id: params[:author_id])
+    book.to_json
   end
 
   delete "/books/:id" do
+    book = Book.find(params[:id])
+    book.destroy
+    book.to_json
   end
 
   get "/reviews" do
+    reviews = Review.all
+    reviews.to_json(include: { user: { only: [:id, :user_name] }, book: { only: [:id, :title] } })
   end
 
   get "/reviews/:id" do
+    review = Review.find(params[:id])
+    review.to_json()
   end
 
   post "/reviews" do
+    user = User.find_or_create_by(user_name: params[:username], email: params[:email])
+    review = Review.create(
+      comment: params[:comment],
+      book_id: params[:book_id],
+      user_id: user.id,
+      star_rating: params[:star_rating],
+    )
+    review.to_json(include: { user: { only: [:id, :user_name] } })
   end
 
   patch "/reviews/:id" do
+    review = Review.find(params[:id])
+    review.update(
+      comment: params[:comment],
+      star_rating: params[:star_rating],
+    )
+    review.to_json(include: { user: { only: [:id, :user_name] } })
   end
 
   delete "/reviews/:id" do
+    review = Review.find(params[:id])
+    review.destroy
+    review.to_json
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,31 +1,56 @@
-puts "🌱 Seeding spices..."
+puts "🌱 Seeding..."
 Book.destroy_all
 Author.destroy_all
+Review.destroy_all
 # Seed your database here
+book_images = ["https://brittlepaper.com/wp-content/uploads/2022/06/mary-alice-A-Coastline-Is-an-Immeasurable-Thing-scaled.jpeg",
+               "https://brittlepaper.com/wp-content/uploads/2022/06/glory-bulawayo.jpeg",
+               "https://brittlepaper.com/wp-content/uploads/2022/06/bitter-akwaeke-emezi.jpeg",
+               "https://brittlepaper.com/wp-content/uploads/2022/06/one-for-sorrow-two-for-joy-9780861542338_hr.jpeg",
+               "https://brittlepaper.com/wp-content/uploads/2022/06/vagabond-osunde.jpeg",
+               "https://brittlepaper.com/wp-content/uploads/2022/06/patience-is-a-subtle-thief.jpeg",
+               "https://brittlepaper.com/wp-content/uploads/2022/06/nikki-may-wahala-2.jpeg",
+               "https://brittlepaper.com/wp-content/uploads/2022/06/scent-of-burnt-flowers.jpeg",
+               "https://brittlepaper.com/wp-content/uploads/2021/12/Untitled-design-2.jpg",
+               "https://brittlepaper.com/wp-content/uploads/2022/06/the-beautiful-side-of-the-moon.jpeg"]
 
 # Create random record names for author
 10.times do
+  
   Author.create(
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
+    name: Faker::Name.name,
   )
 end
 
 # Create random names for users
+15.times do
+  User.create(
+    user_name: Faker::Name.unique.name,
+    email: Faker::Internet.email,
+  )
+end
 
 # Creae book records
-50.times do
+10.times do |i|
   author = Author.order("RANDOM()").first
-  Book.create(
+  book = Book.create(
 
     title: Faker::Book.title,
     category: Faker::Book.genre,
-    description: Faker::Lorem.paragraph,
+    description: Faker::Lorem.paragraph(sentence_count: 50),
     published_date: Faker::Date.between_except(from: 1.year.ago, to: 1.year.from_now, excepted: Date.today),
-    image_url: Faker::Avatar.image,
+    image_url: book_images[i],
     author_id: author.id,
-    # Add 5 reviews for each book
   )
+  # Add 5 reviews for each book
+  5.times do
+    user_id = User.order("RANDOM()").first.id
+    book.reviews << Review.create(
+      comment: Faker::Lorem.paragraph,
+      star_rating: rand(1...5),
+      user_id: user_id,
+    )
+  end
 end
 
 #

fe5a722bbe59c3a0df5d2106dfc4776027cad60e
Murage-Ivy <ivy.murage@student.moringaschool.com>
2022-11-11 14:08:17 +0300

    Deploy backend

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_11_06_090556) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "authors", force: :cascade do |t|
     t.string "name"
   end
